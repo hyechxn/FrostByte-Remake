7d7e74cd3eaba0736f0245ebe43d2993e8f28297
hyechxn <a49918072@gmail.com>
2024-07-23 22:49:56 +0900

    Reset

---
+++ JumboTime/Assets/2.Scripts/Move.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class Move : MonoBehaviour
+{
+    public void What()
+    {
+        Debug.Log("dsanagjdk");
+    }
+    public void OnType()
+    {
+        Debug.Log("dadas");
+    }
+
+}
